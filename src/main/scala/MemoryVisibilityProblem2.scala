79618566e85383b6206f2a9d59671cbc7818a4e9
Ryusuke Iida <iida-ryusuke469@g.ecc.u-tokyo.ac.jp>
2020-03-16 09:44:53 +0900

    synchronizedブロックを利用した可視性の確保

--- src/main/scala/MemoryVisibilityProblem2.scala
+++ src/main/scala/MemoryVisibilityProblem2.scala
@@ -1,15 +1,15 @@
-object MemoryVisibilityProblem2 extends App {
-  var runner = new AsyncRunner2("Runner 0", () => true)
+object MemoryVisibilityProblem1 extends App {
+  var runner = new AsyncRunner1("Runner 0", () => true)
 
   for (i <- 1 to 10) {
     runner.asyncRun((name) => println(s"${name} is finished."))
-    runner = new AsyncRunner2(s"Runner ${i}", runner.canNextStart)
+    runner = new AsyncRunner1(s"Runner ${i}", runner.canNextStart)
   }
 
   runner.asyncRun((name) => println(s"${name} is finished. Totally finished."))
 }
 
-class AsyncRunner2(private[this] val name: String, private[this] val canStart: () => Boolean) {
+class AsyncRunner1(private[this] val name: String, private[this] val canStart: () => Boolean) {
   private[this] var isFinished = false
 
   def asyncRun(f: String => Unit): Unit = {
@@ -18,9 +18,11 @@ class AsyncRunner2(private[this] val name: String, private[this] val canStart: (
         Thread.`yield`()
       }
       f(name)
-      isFinished = true
+      setIsFinished(true)
     }).start()
   }
 
-  def canNextStart: () => Boolean = () => this.isFinished
+  def setIsFinished(bool: Boolean) = synchronized { isFinished = true }
+  def canNextStart: () => Boolean = () => synchronized{ this.isFinished }
 }
+
