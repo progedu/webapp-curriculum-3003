4afff96c54a3dfc2a6df235efb25f8ea791e08ad
JojiKoike <joji.koike@gmail.com>
2020-06-23 00:37:32 +0900

    スタック拘束にスレッドセーフ化

--- src/main/scala/ThreadSafeFormatter.scala
+++ src/main/scala/ThreadSafeFormatter.scala
@@ -1,7 +1,11 @@
+import java.text.SimpleDateFormat
 import java.util.Date
 
 object ThreadSafeFormatter {
 
-  def format(date: Date) = ???
+  def format(date: Date): String = {
+    val formatter = new SimpleDateFormat("yyyy'年'MM'月'dd'日'E'曜日'H'時'mm'分'")
+    formatter.format(date)
+  }
 
 }
