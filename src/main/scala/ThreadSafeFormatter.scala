3b1e010786552168b9d867298028be24dbab7db1
YuuTakimotoY <44459676+YuuTakimotoY@users.noreply.github.com>
2020-06-24 00:30:31 +0900

    スレッドセーフに実装

--- src/main/scala/ThreadSafeFormatter.scala
+++ src/main/scala/ThreadSafeFormatter.scala
@@ -2,6 +2,9 @@ import java.util.Date
 
 object ThreadSafeFormatter {
 
-  def format(date: Date) = ???
+  def format(date: Date) = {
+    val formatter = new SimpleDateFormat("yyyy'年'MM'月'dd'日'E'曜日'H'時'mm'分'")
+    formatter.format(date)
+  }
 
 }
