59727c79b8f8732e6b9b7a3ceafd09e716c95198
QWYNG <ikusawasi@gmail.com>
2021-04-24 17:43:03 +0900

    problem3

--- src/main/scala/ThreadSafeFormatter.scala
+++ src/main/scala/ThreadSafeFormatter.scala
@@ -1,7 +1,11 @@
+import java.text.SimpleDateFormat
 import java.util.Date
 
 object ThreadSafeFormatter {
 
-  def format(date: Date) = ???
+  def format(date: Date): String = {
+    val formatter = new SimpleDateFormat("yyyy年MM月dd日E曜日H時mm分")
+    formatter.format(date)
+  }
 
 }
